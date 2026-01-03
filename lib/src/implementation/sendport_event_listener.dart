7bd66c4825408f815dfbd69a0d30304abb4e6c93
Nick Fisher <nick.fisher@avinium.com>
2026-01-03 14:46:29 +0800

    simplify collision callback data

--- lib/src/implementation/sendport_event_listener.dart
+++ lib/src/implementation/sendport_event_listener.dart
@@ -1,11 +1,9 @@
 import 'dart:ffi' as ffi;
 import 'dart:isolate';
-import 'dart:typed_data';
 import 'package:vector_math/vector_math_64.dart';
 import '../bindings/src/bindings.dart';
 import '../interfaces/collision_callback.dart';
 import '../interfaces/event_listener.dart';
-import '../interfaces/physics_world.dart';
 import 'ffi_rigid_body.dart';
 import 'ffi_collider.dart';
 
@@ -91,34 +89,44 @@ class SendPortEventListener implements EventListener {
 
   /// Process a binary message from native code
   void _processMessage(ffi.Pointer<ffi.Uint8> data, int size) {
-    final reader = _MessageReader(data, size);
+    final bytes = data.asTypedList(size);
+    final byteData = ByteData.view(bytes.buffer);
 
     // Read message type
-    final messageType = reader.readUint32();
+    int offset = 0;
+    final messageType = byteData.getUint32(offset, Endian.little);
+    offset += 4;
 
     if (messageType == 0) {
       // Contact data
-      _processContactData(reader);
+      _processContactData(byteData, offset);
     } else if (messageType == 1) {
       // Overlap data
-      _processOverlapData(reader);
+      _processOverlapData(byteData, offset);
     }
   }
 
   /// Process contact data from the message
-  void _processContactData(_MessageReader reader) {
-    final nbPairs = reader.readUint32();
+  void _processContactData(ByteData byteData, int initialOffset) {
+    int offset = initialOffset;
+    final nbPairs = byteData.getUint32(offset, Endian.little);
+    offset += 4;
     final contactPairs = <ContactPair>[];
 
     for (int i = 0; i < nbPairs; i++) {
       // Read body/collider addresses
-      final body1Addr = reader.readUint64();
-      final body2Addr = reader.readUint64();
-      final collider1Addr = reader.readUint64();
-      final collider2Addr = reader.readUint64();
+      final body1Addr = byteData.getUint64(offset, Endian.little);
+      offset += 8;
+      final body2Addr = byteData.getUint64(offset, Endian.little);
+      offset += 8;
+      final collider1Addr = byteData.getUint64(offset, Endian.little);
+      offset += 8;
+      final collider2Addr = byteData.getUint64(offset, Endian.little);
+      offset += 8;
 
       // Read event type
-      final eventTypeInt = reader.readInt32();
+      final eventTypeInt = byteData.getInt32(offset, Endian.little);
+      offset += 4;
       final ContactEventType eventType;
       if (eventTypeInt == 0) {
         eventType = ContactEventType.contactStart;
@@ -129,26 +137,37 @@ class SendPortEventListener implements EventListener {
       }
 
       // Read contact points
-      final nbPoints = reader.readUint32();
+      final nbPoints = byteData.getUint32(offset, Endian.little);
+      offset += 4;
       final contactPoints = <ContactPoint>[];
 
       for (int j = 0; j < nbPoints; j++) {
-        final penetrationDepth = reader.readFloat();
-        final normalX = reader.readFloat();
-        final normalY = reader.readFloat();
-        final normalZ = reader.readFloat();
-        final point1X = reader.readFloat();
-        final point1Y = reader.readFloat();
-        final point1Z = reader.readFloat();
-        final point2X = reader.readFloat();
-        final point2Y = reader.readFloat();
-        final point2Z = reader.readFloat();
+        final penetrationDepth = byteData.getFloat32(offset, Endian.little);
+        offset += 4;
+        final normalX = byteData.getFloat32(offset, Endian.little);
+        offset += 4;
+        final normalY = byteData.getFloat32(offset, Endian.little);
+        offset += 4;
+        final normalZ = byteData.getFloat32(offset, Endian.little);
+        offset += 4;
+        final point1X = byteData.getFloat32(offset, Endian.little);
+        offset += 4;
+        final point1Y = byteData.getFloat32(offset, Endian.little);
+        offset += 4;
+        final point1Z = byteData.getFloat32(offset, Endian.little);
+        offset += 4;
+        final point2X = byteData.getFloat32(offset, Endian.little);
+        offset += 4;
+        final point2Y = byteData.getFloat32(offset, Endian.little);
+        offset += 4;
+        final point2Z = byteData.getFloat32(offset, Endian.little);
+        offset += 4;
 
         contactPoints.add(ContactPoint(
-          penetrationDepth: penetrationDepth.toDouble(),
-          worldNormal: Vector3(normalX.toDouble(), normalY.toDouble(), normalZ.toDouble()),
-          localPointOnCollider1: Vector3(point1X.toDouble(), point1Y.toDouble(), point1Z.toDouble()),
-          localPointOnCollider2: Vector3(point2X.toDouble(), point2Y.toDouble(), point2Z.toDouble()),
+          penetrationDepth: penetrationDepth,
+          worldNormal: Vector3(normalX, normalY, normalZ),
+          localPointOnCollider1: Vector3(point1X, point1Y, point1Z),
+          localPointOnCollider2: Vector3(point2X, point2Y, point2Z),
         ));
       }
 
@@ -174,20 +193,25 @@ class SendPortEventListener implements EventListener {
   }
 
   /// Process overlap data from the message
-  void _processOverlapData(_MessageReader reader) {
-    final nbPairs = reader.readUint32();
+  void _processOverlapData(ByteData byteData, int initialOffset) {
+    int offset = initialOffset;
+    final nbPairs = byteData.getUint32(offset, Endian.little);
+    offset += 4;
 
     // For now, overlaps are reported as contact pairs with no points
     // In a full implementation, you might want a separate callback for overlaps
     print('Received $nbPairs overlap events');
 
     for (int i = 0; i < nbPairs; i++) {
-      final body1Addr = reader.readUint64();
-      final body2Addr = reader.readUint64();
-      final eventTypeInt = reader.readInt32();
+      final body1Addr = byteData.getUint64(offset, Endian.little);
+      offset += 8;
+      final body2Addr = byteData.getUint64(offset, Endian.little);
+      offset += 8;
+      final eventTypeInt = byteData.getInt32(offset, Endian.little);
+      offset += 4;
 
       // Skip points (overlaps have none)
-      reader.readUint32();
+      offset += 4;
 
       print('  Overlap between bodies at $body1Addr and $body2Addr: '
             '${eventTypeInt == 0 ? "Start" : eventTypeInt == 1 ? "Stay" : "Exit"}');
@@ -213,104 +237,4 @@ class SendPortEventListener implements EventListener {
     rp3d_destroy_event_listener(_listenerPtr);
     calloc.free(_messageBuffer);
   }
-}
-
-/// Helper class for reading binary messages from native code
-class _MessageReader {
-  final ffi.Pointer<ffi.Uint8> _data;
-  int _offset;
-
-  _MessageReader(this._data, int size) : _offset = 0;
-
-  int readUint32() {
-    final value = _data.elementAt(_offset).cast<ffi.Uint32>().value;
-    _offset += 4;
-    return value;
-  }
-
-  int readInt32() {
-    final value = _data.elementAt(_offset).cast<ffi.Int32>().value;
-    _offset += 4;
-    return value;
-  }
-
-  int readUint64() {
-    final value = _data.elementAt(_offset).cast<ffi.Uint64>().value;
-    _offset += 8;
-    return value;
-  }
-
-  double readFloat() {
-    final value = _data.elementAt(_offset).cast<ffi.Float>().value;
-    _offset += 4;
-    return value.toDouble();
-  }
-
-  bool get hasMore => true; // We don't track the original size
-}
-
-/// Helper function to create a buffer for testing SendPort communication
-///
-/// This can be used to test the SendPort integration without needing
-/// actual collisions to occur.
-ffi.Pointer<ffi.Uint8> createTestMessageBuffer() {
-  // Create a simple test message with one contact pair
-  const size = 4 + // message type
-      4 + // nb pairs
-      8 + 8 + 8 + 8 + // pointers
-      4 + // event type
-      4 + // nb points
-      1 * 4 + 3 * 4 + 3 * 4 + 3 * 4; // one point
-
-  final buffer = calloc.allocate<ffi.Uint8>(size);
-  final writer = _MessageWriter(buffer);
-
-  writer.writeUint32(0); // Message type: contact data
-  writer.writeUint32(1); // 1 pair
-
-  // Write a dummy pair
-  writer.writeUint64(0x1000); // body1
-  writer.writeUint64(0x2000); // body2
-  writer.writeUint64(0x3000); // collider1
-  writer.writeUint64(0x4000); // collider2
-  writer.writeInt32(0); // event type: start
-  writer.writeUint32(1); // 1 point
-
-  // Write a dummy point
-  writer.writeFloat(1.0); // penetration depth
-  writer.writeFloat(0.0); writer.writeFloat(1.0); writer.writeFloat(0.0); // normal
-  writer.writeFloat(0.0); writer.writeFloat(0.0); writer.writeFloat(0.0); // point1
-  writer.writeFloat(0.0); writer.writeFloat(0.0); writer.writeFloat(0.0); // point2
-
-  return buffer;
-}
-
-/// Helper class for writing binary messages to native buffers
-class _MessageWriter {
-  final ffi.Pointer<ffi.Uint8> _data;
-  int _offset;
-
-  _MessageWriter(this._data) : _offset = 0;
-
-  void writeUint32(int value) {
-    _data.elementAt(_offset).cast<ffi.Uint32>().value = value;
-    _offset += 4;
-  }
-
-  void writeInt32(int value) {
-    _data.elementAt(_offset).cast<ffi.Int32>().value = value;
-    _offset += 4;
-  }
-
-  void writeUint64(int value) {
-    _data.elementAt(_offset).cast<ffi.Uint64>().value = value;
-    _offset += 8;
-  }
-
-  void writeFloat(double value) {
-    _data.elementAt(_offset).cast<ffi.Float>().value = value;
-    _offset += 4;
-  }
-
-  int get offset => _offset;
 }
